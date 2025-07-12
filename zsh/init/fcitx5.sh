b779892fa9c160a62cebaa4c810d5d3f6e662a28
sc <0x1247c14@gmail.com>
2025-07-12 18:11:47 +0800

    style(zsh): Modified the permission of scripts, not load fcitx env while not in X11.

--- zsh/init/fcitx5.sh
+++ zsh/init/fcitx5.sh
@@ -1,9 +1,13 @@
 #!/bin/bash
+# @author sc
+# @scince 2025/7/12
 
-export XIM="fcitx"
-export XIM_PROGRAM="fcitx"
-export XMODIFIERS="@im=fcitx"
-export QT_IM_MODULE="fcitx"
+# This script sets up the environment for Fcitx5
+if [[ -n "$DISPLAY" ]]; then
+    export XIM="fcitx"
+    export XIM_PROGRAM="fcitx"
+    export XMODIFIERS="@im=fcitx"
+    export QT_IM_MODULE="fcitx"
+    export GTK_IM_MODULE="fcitx"
+fi
 
-# X11
-export GTK_IM_MODULE="fcitx"
